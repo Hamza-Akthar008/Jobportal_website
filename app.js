70a49503f40d5bf9423aa56201f715aaea708eac
Hamza-Akthar008 <f200319@cfd.nu.edu.pk>
2022-11-25 20:27:06 +0500

    jobportal_webiste commit

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express =require('express');
 const app =express();
 const routes =require('./routes/index')
 var bodyparser=require('body-parser');
-const cookieParser = require("cookie-parser");
-var session=require('express-session');
+
 
 app.use(cookieParser());
 app.set("view engine","ejs");
@@ -14,9 +13,5 @@ app.use('/img',express.static('public/img'));
 app.use(bodyparser.json());
  app.use(bodyparser.urlencoded({extended:true}));
 app.use('/',routes);
-app.use(session({
-	secret: 'iamhamzarajajlalgorayaabdullahkhan',
-	resave: true,
-	saveUninitialized: true
-}));
+
 app.listen(3000,()=>console.log("Listening to port 3000"));
