26b44d16e726c95750b2b7271fc0985625ce60fe
Hamza-Akthar008 <f200319@cfd.nu.edu.pk>
2022-11-30 00:36:49 +0500

    comments Functionality commit

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -294,8 +294,12 @@ exports.blog = (req, res) => {
 var query ="Select * from blog";
 con.query(query,(err,data)=>
 {
+var query ="Select * from comments";
+con.query(query,(err,result)=>
+{
+    res.render('blog',{data:data,result:result});
 
-    res.render('blog',{data:data});
+})
 })
 
 
